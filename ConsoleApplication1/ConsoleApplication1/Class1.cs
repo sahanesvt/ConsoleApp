916a5a307be69c850c4be1a806c441ee1ddcae68
sahanesvt <sahanes_2000@yahoo.com>
2016-06-28 00:22:46 -0400

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Class1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * These are some comments
+ * 
+ */
+
+
+namespace BeamProperties
+{
+    public class Properties
+    {
+        // beam dimension variables
+        public static double bft, tft, D, tw, bfb, tfb;
+        // beam property variables
+        public static double Atf, Aweb, Abf, Ytf, Yweb, Ybf, Itf, Iweb, Ibf, area, I, CG, NA, Stop, Sbot, PNA, PCGtop, PCGbot, Z;
+        // beam property calculations
+        public static double PartArea(double width, double height)
+        {
+            area = width*height;
+            return area;
+        }
+        public static double PartCG(double first, double second, double last)
+        {
+            CG = first+second+last/2;
+            return CG;
+        }       
+        public static double BeamArea(double bft, double tft, double D, double tw, double bfb, double tfb)
+        {
+            Atf = Properties.PartArea(bft, tft);
+            Aweb = Properties.PartArea(D, tw);
+            Abf = Properties.PartArea(bfb, tfb);
+            area = Atf + Aweb + Abf;
+            return area;
+        }
+        public static double NeutralAxis(double bft, double tft, double D, double tw, double bfb, double tfb)
+        {
+            Atf = Properties.PartArea(bft, tft);
+            Aweb = Properties.PartArea(D, tw);
+            Abf = Properties.PartArea(bfb, tfb);
+            Ytf = Properties.PartCG(tfb, D, tft);
+            Yweb = Properties.PartCG(tfb, 0, D);
+            Ybf = Properties.PartCG(0, 0, tfb);
+            NA = (Atf * Ytf + Aweb * Yweb + Abf * Ybf) / (Atf + Aweb + Abf);
+            return NA; 
+        }
+        public static double PartMomOfInert(double width, double height)
+        {
+            I = width * Math.Pow(height,3)/12;
+            return I;
+        }        
+        public static double MomentOfIneria(double bft, double tft, double D, double tw, double bfb, double tfb)
+        {
+            Atf = Properties.PartArea(bft, tft);
+            Aweb = Properties.PartArea(D, tw);
+            Abf = Properties.PartArea(bfb, tfb);
+            Ytf = Properties.PartCG(tfb, D, tft);
+            Yweb = Properties.PartCG(tfb, 0, D);
+            Ybf = Properties.PartCG(0, 0, tfb);
+            NA = Properties.NeutralAxis(bft, tft, D, tw, bfb, tfb);
+            Itf = Properties.PartMomOfInert(bft, tft);
+            Iweb = Properties.PartMomOfInert(tw, D);
+            Ibf = Properties.PartMomOfInert(bfb, tfb);
+            I = Itf + Atf * Math.Pow(Ytf - NA, 2) + Iweb + Aweb * Math.Pow(Yweb - NA, 2) + Ibf + Abf * Math.Pow(Ybf - NA, 2);
+            return I;
+        }
+        public static double ElastSectModTop(double bft, double tft, double D, double tw, double bfb, double tfb)
+        {
+            Ytf = Properties.PartCG(tfb, D, tft);
+            NA = Properties.NeutralAxis(bft, tft, D, tw, bfb, tfb);
+            I = Properties.MomentOfIneria(bft, tft, D, tw, bfb, tfb);
+            Stop = I / (Ytf - NA + tft / 2);
+            return Stop;
+        }
+        public static double ElastSectModBot(double bft, double tft, double D, double tw, double bfb, double tfb)
+        {
+            Ybf = Properties.PartCG(0, 0, tfb);
+            NA = Properties.NeutralAxis(bft, tft, D, tw, bfb, tfb);
+            I = Properties.MomentOfIneria(bft, tft, D, tw, bfb, tfb);            
+            Sbot = I / (NA - Ybf + tfb / 2);
+            return Sbot;
+        }
+        public static double PlastNeutralAxis(double bft, double tft, double D, double tw, double bfb, double tfb)
+        {
+            PNA = tfb + (D + (bft * tft - bfb * tfb) / tw) / 2;
+            return PNA;
+        }
+        public static double PNAtoTopCG(double bft, double tft, double D, double tw, double bfb, double tfb)
+        {
+            area = Properties.BeamArea(bft, tft, D, tw, bfb, tfb);
+            PNA = Properties.PlastNeutralAxis(bft, tft, D, tw, bfb, tfb);
+            PCGtop = (bft * tft * (tft / 2 + D + tfb - PNA) + tw * Math.Pow(D - PNA + tfb, 2) / 2) * 2 / area;
+            return PCGtop;
+        }
+        public static double PNAtoBotCG(double bft, double tft, double D, double tw, double bfb, double tfb)
+        {
+            area = Properties.BeamArea(bft, tft, D, tw, bfb, tfb);
+            PNA = Properties.PlastNeutralAxis(bft, tft, D, tw, bfb, tfb);
+            PCGbot = (bfb * tfb * (PNA - tfb / 2) + tw * Math.Pow(PNA - tfb, 2) / 2) * 2 / area;
+            return PCGbot;
+        }        
+        public static double PlastSectMod(double bft, double tft, double D, double tw, double bfb, double tfb)
+        {
+            area = Properties.BeamArea(bft, tft, D, tw, bfb, tfb);
+            PNA = Properties.PlastNeutralAxis(bft, tft, D, tw, bfb, tfb);
+            PCGtop = Properties.PNAtoTopCG(bft, tft, D, tw, bfb, tfb);
+            PCGbot = Properties.PNAtoBotCG(bft, tft, D, tw, bfb, tfb);
+            Z = area / 2 * (PCGtop + PCGbot);
+            return Z;
+        }
+    }
+
+    public class Beams
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number of Beams : ");
+            int NoBeams = Convert.ToInt32(Console.ReadLine());
+            double[,] table;
+            table = new double[7, NoBeams];
+            for (int i = 0; i < NoBeams; i++)
+            {
+                Console.WriteLine("Enter Beam {0} top flange width, bft (in) : ",i + 1);
+                double bft = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter Beam {0} top flange thickness, tft (in) : ", i + 1);
+                double tft = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter Beam {0} web depth, D (in) : ", i + 1);
+                double D = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter Beam {0} web thickness, tw (in) : ", i + 1);
+                double tw = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter Beam {0} bottom flange width, bfb (in) : ", i + 1);
+                double bfb = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Enter Beam {0} bottom flange thickness, tfb (in) : ", i + 1);
+                double tfb = Convert.ToDouble(Console.ReadLine());
+                double area = Properties.BeamArea(bft, tft, D, tw, bfb, tfb);
+                table[0, i] = area;
+                double NA = Properties.NeutralAxis(bft, tft, D, tw, bfb, tfb);
+                table[1, i] = NA;
+                double I = Properties.MomentOfIneria(bft, tft, D, tw, bfb, tfb);
+                table[2, i] = I;
+                double Stop = Properties.ElastSectModTop(bft, tft, D, tw, bfb, tfb);
+                table[3, i] = Stop;
+                double Sbot = Properties.ElastSectModBot(bft, tft, D, tw, bfb, tfb);
+                table[4, i] = Sbot;
+                double PNA = Properties.PlastNeutralAxis(bft, tft, D, tw, bfb, tfb);
+                table[5, i] = PNA;
+                double Z = Properties.PlastSectMod(bft, tft, D, tw, bfb, tfb);
+                table[6, i] = Z;
+            }
+                for (int j = 0; j < NoBeams; j++)
+                {
+                    Console.WriteLine("*********** BEAM {0} SECTION PROPERTIES***********", j + 1);
+                    Console.WriteLine("Area of Beam is {0} in^2", Math.Round(table[0, j], 2));
+                    Console.WriteLine("Elastic Neutral Axis is {0} in", Math.Round(table[1, j], 2));
+                    Console.WriteLine("Elastic Moment of Inertia is {0} in^4", Math.Round(table[2, j], 2));
+                    Console.WriteLine("Elastic Section Modulus of Top Flange is {0} in^3", Math.Round(table[3, j], 2));
+                    Console.WriteLine("Elastic Section Modulus of Bottom Flange is {0} in^3", Math.Round(table[4, j], 2));
+                    Console.WriteLine("Plastic Neutral Axis is {0} in", Math.Round(table[5, j], 2));
+                    Console.WriteLine("Plastic Section Modulus is {0} in^3", Math.Round(table[6, j], 2));
+                    Console.WriteLine("");
+                }
+                Console.Read();
+        }
+    }
+}
+
